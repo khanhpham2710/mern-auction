cd13e9a155ca2885a3660984892b88ee833a3381
phamduykhanh98 <134706244+phamduykhanh98@users.noreply.github.com>
2025-01-04 21:13:52 +0700

    change domain

--- backend/app.js
+++ backend/app.js
@@ -60,9 +60,9 @@ connection();
 app.use(errorMiddleware);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(_dirname,"/frontend/dist")));
+	app.use(express.static(path.join(__dirname,"/frontend/dist")));
 	app.use("*",(_,res) => {
-        res.sendFile(path.resolve(_dirname, "frontend","dist","index.html"));
+        res.sendFile(path.resolve(__dirname, "frontend","dist","index.html"));
     });
 }
 
