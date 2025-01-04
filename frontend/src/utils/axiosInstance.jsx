e8731e53ddad011825c8ed175e75de6a0a0f9910
phamduykhanh98 <134706244+phamduykhanh98@users.noreply.github.com>
2025-01-04 22:49:29 +0700

    fix login

--- frontend/src/utils/axiosInstance.jsx
+++ frontend/src/utils/axiosInstance.jsx
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+// https://mern-auction-yjtj.onrender.com/auth/login
+
 const axiosInstance = axios.create({
-  baseURL: "https://mern-auction-yjtj.onrender.com/api/v1",
+  baseURL: "http://localhost:5000/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
@@ -9,15 +11,16 @@ const axiosInstance = axios.create({
 
 
 axiosInstance.interceptors.response.use(
-  (response) => response,
+  (response) => {
+    return response;
+  },
   async (error) => {
     const originalRequest = error.config;
-
-    if (error.response.status === 401 && !originalRequest._retry) {
+    if (error.response && (error.response.status === 401) && !originalRequest._retry) {
       originalRequest._retry = true;
       try {
-        await axiosInstance.post(
-          "/user/refresh",
+        await axios.post(
+          "http://localhost:5000/api/v1/user/refresh",
           {},
           {
             withCredentials: true,
@@ -25,7 +28,7 @@ axiosInstance.interceptors.response.use(
         );
         return axiosInstance(originalRequest);
       } catch (refreshError) {
-        console.error("Token refresh failed:", refreshError);
+        // console.error("Token refresh failed:", refreshError);
         // window.location.href = 'http://localhost:5173/auth/login';
         return Promise.reject(refreshError);
       }
