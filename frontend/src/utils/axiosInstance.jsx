f084d916e3e9afa782f905a1464cce311012c70c
phamduykhanh98 <134706244+phamduykhanh98@users.noreply.github.com>
2025-01-05 08:28:25 +0700

    fix email

--- frontend/src/utils/axiosInstance.jsx
+++ frontend/src/utils/axiosInstance.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // https://mern-auction-yjtj.onrender.com/auth/login
 const axiosInstance = axios.create({
-  baseURL: "https://mern-auction-yjtj.onrender.com//api/v1",
+  baseURL: "https://mern-auction-yjtj.onrender.com/api/v1",
   // baseURL: "http://localhost:5000/api/v1",
   headers: {
     "Content-Type": "application/json",
